cd9f2bcb380facf5f4440416d2400a7c7825138f
ParmbeerJohal <parmbeerjohal@gmail.com>
2020-06-29 16:55:03 -0700

    Finished frontend form feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 
 const url = "http://localhost:8000";
@@ -7,6 +7,7 @@ const url = "http://localhost:8000";
 function App() {
   const [users, setUsers] = useState();
   const [isFetching, setFetching] = useState(false);
+
 
   const fetchUsers = async () => {
     setFetching(true);
@@ -32,9 +33,91 @@ function App() {
     fetchUsers();
   }, []);
 
+
+  //Starting 'add user' feature
+  function refreshPage() {
+    window.location.reload(false);
+  }
+
+  function AddPersonForm() {
+    const [user, setUser] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+
+    function handleUserChange(e) {
+      setUser(e.target.value);
+    }
+
+    function handleEmailChange(e) {
+      setEmail(e.target.value);
+    }
+
+    function handlePhoneChange(e) {
+      setPhone(e.target.value);
+    }
+
+    function handleSubmit(e) {
+      setFetching(true);
+
+      //Using fetch API to post form data to django backend
+      fetch(url + "/users/add/",{
+        method: "post",
+        body: JSON.stringify({"name": user, "email": email, "phone": phone})
+      }).then(function(response) {
+          return response.json();
+      }).then(function(data) {
+          console.log("Data:", data);
+      }).catch(function(ex) {
+          console.log("fail:", ex);
+      });
+
+      //After data is submitted, clear form input fields
+      setUser('');
+      setEmail('');
+      setPhone('');
+
+      e.preventDefault();
+      setFetching(false);
+    }
+    return (
+      <div>
+        <form onSubmit={handleSubmit} action={url+"/users/add/"} method="POST">
+          <label for="name">Name *:</label>
+          <input type="text"
+          placeholder="Add Full Name"
+          onChange={handleUserChange}
+          name="name"
+          id="name"
+          value={user}/>
+          <br/>
+          <label for="email">Email :</label>
+          <input type="text"
+          placeholder="example@example.com"
+          onChange={handleEmailChange}
+          name="email"
+          id="email"
+          value={email}/>
+          <br/>
+          <label for="phone">Phone :</label>
+          <input type="text"
+          placeholder="eg. 999-999-9999"
+          onChange={handlePhoneChange}
+          name="phone"
+          id="phone"
+          value={phone}/>
+          <br/>
+          <button type="submit" class="add-btn" onClick={refreshPage}>Add user</button>
+        </form>
+        <button onClick={refreshPage} class="cancel-btn">Cancel</button>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <AddPersonForm />
+        <h2>Users:</h2>
         {isFetching ? (
           <p>Loading...</p>
         ) : (
